a47744e874ca6a6f2dd0723371438c3aead1357c
Mihail Seregin <mihail.mieke@yandex.ru>
2022-10-18 22:03:40 +0300

    corrected all comments

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -3,10 +3,6 @@ package utils;
 import config.Config;
 
 public class Constants extends Config {
-/*    protected static final String EMAIL_TEST = "mihail777mieke334@yandex.ru";
-    protected static final String PASSWORD_TEST = "password_test";
-    protected static final String NAME_TEST = "Username_test";*/
-
     protected static final String API_CREATE_USER = "api/auth/register";
     protected static final String API_AUTHORIZATION_USER ="api/auth/login";
     protected static final String API_INFO = "api/auth/user";
